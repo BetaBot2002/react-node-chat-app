d2e35b0f01d57b6ca3bebc6de739678ebcdc9998
Sinchan Nandy <105705266+BetaBot2002@users.noreply.github.com>
2023-10-06 18:07:32 +0530

    Chakra Ui installed

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,12 +1,13 @@
 import { useState } from 'react'
 import './App.css'
-
+import { Button, ChakraProvider } from '@chakra-ui/react'
 function App() {
 
   return (
-    <>
+    <ChakraProvider>
       <h1>Hello World</h1>
-    </>
+      <Button colorScheme='blue'>Button</Button>
+    </ChakraProvider>
   )
 }
 
