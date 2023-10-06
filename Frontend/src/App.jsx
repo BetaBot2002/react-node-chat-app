7c2bea4274e8bb04135dc979481e83b9c6aff6bd
Sinchan Nandy <105705266+BetaBot2002@users.noreply.github.com>
2023-10-06 18:21:53 +0530

    Routes Initiated, Home Component added

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,11 +1,13 @@
 import { useState } from 'react'
 import './App.css'
+import { Route, Routes } from 'react-router-dom'
+import Home from './Components/Home'
 function App() {
 
   return (
-    <>
-      <h1>Hello World</h1>
-    </>
+    <Routes>
+      <Route path='/' element={<Home />}/>
+    </Routes>
   )
 }
 
