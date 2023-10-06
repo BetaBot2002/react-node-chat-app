a6c8573a18331c2536157f6b92d3d361e267de35
Sinchan Nandy <105705266+BetaBot2002@users.noreply.github.com>
2023-10-07 02:30:13 +0530

    Login page style initiated

--- Frontend/src/Components/LoginSignup.jsx
+++ Frontend/src/Components/LoginSignup.jsx
@@ -1,10 +1,30 @@
 import React from 'react'
 import '../CSS/LoginSignup.css'
+import {Colors} from '../Utils/CSS-Variables.js'
+import { Box, Container, Text } from '@chakra-ui/react'
 const LoginSignup = () => {
   return (
-    <div className='LoginSignUp-Body'>
-
-    </div>
+    <Container maxW={'xl'} centerContent>
+      <Box
+        display={'flex'}
+        justifyContent={'center'}
+        alignItems={'center'}
+        bg={Colors.theme_light_grey}
+        p={3}
+        w={'100%'}
+        m={'40px 0 15px 0'}
+        borderRadius={'lg'}
+        borderWidth={'2px'}
+        borderColor={Colors.theme_lavender}
+      >
+        <Text
+          fontSize={'4xl'}
+          fontFamily={'Helvetica'}
+          fontWeight={'bold'}
+          letterSpacing={'2px'}
+        >Chattery</Text>
+      </Box>
+    </Container>
   )
 }
 
---
+++ Frontend/src/Utils/CSS-Variables.js
@@ -0,0 +1,11 @@
+const Colors={
+    theme_dark:'#1e1e1e',
+    theme_light_grey:'#323233',
+    theme_bold_red:'#f2432e',
+    theme_light:'rgb(245, 245, 226)',
+    theme_lavender:'#d6bcfa'
+}
+
+export {
+    Colors
+}
