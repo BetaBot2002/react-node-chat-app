a26854480e87ec79e69321b00f0295bc130bc98c
Sinchan Nandy <105705266+BetaBot2002@users.noreply.github.com>
2023-10-07 01:45:44 +0530

    LoginSignup component initiated

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -2,11 +2,13 @@ import { useState } from 'react'
 import './App.css'
 import { Route, Routes } from 'react-router-dom'
 import Home from './Components/Home'
+import LoginSignup from './Components/LoginSignup'
 function App() {
 
   return (
     <Routes>
       <Route path='/' element={<Home />}/>
+      <Route path='/registration' element={<LoginSignup />}/>
     </Routes>
   )
 }
---
+++ Frontend/src/Components/LoginSignup.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import '../CSS/LoginSignup.css'
+const LoginSignup = () => {
+  return (
+    <div className='LoginSignUp-Body'>
+
+    </div>
+  )
+}
+
+export default LoginSignup
+
