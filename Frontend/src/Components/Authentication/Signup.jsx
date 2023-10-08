c3fb12d063e833e1118ba9ae1228b0719f58572c
Sinchan Nandy <105705266+BetaBot2002@users.noreply.github.com>
2023-10-08 21:47:39 +0530

    Picture Upload added

--- Frontend/src/Components/Authentication/Signup.jsx
+++ Frontend/src/Components/Authentication/Signup.jsx
@@ -5,10 +5,15 @@ import { Colors } from '../../Utils/CSS-Variables'
 const Signup = () => {
   const [isShown, setIsShown] = useState(false)
   const [isConfirmShown, setIsConfirmShown] = useState(false)
-  const [isConfirmed,setIsConfirmed]=useState(false)
+  const [isConfirmed, setIsConfirmed] = useState(false)
   const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
   const [confirmPassword, setConfirmPassword] = useState('')
+
+  const picUpload = (image) => {
+
+  }
+
   return (
     <VStack spacing={'15px'}>
       <FormControl id='email' isRequired>
@@ -23,18 +28,18 @@ const Signup = () => {
         <FormLabel>Password</FormLabel>
         <InputGroup>
           <Input
-            type={isShown?'text':'password'}
+            type={isShown ? 'text' : 'password'}
             placeholder='Enter Your Password'
             onChange={(e) => {
               setPassword(e.target.value)
-              setIsConfirmed(confirmPassword===e.target.value)
+              setIsConfirmed(confirmPassword === e.target.value)
             }}
             borderColor={Colors.theme_lavender}
           />
           <InputRightElement w={'80px'}>
             <Button
               variant={'unstyled'}
-              onClick={()=>{
+              onClick={() => {
                 setIsShown(!isShown)
               }}
             >
@@ -48,38 +53,47 @@ const Signup = () => {
         <FormLabel>Confirm Password</FormLabel>
         <InputGroup>
           <Input
-            type={isConfirmShown?'text':'password'}
+            type={isConfirmShown ? 'text' : 'password'}
             placeholder='Confirm Your Password'
             onChange={(e) => {
               setConfirmPassword(e.target.value)
-              setIsConfirmed(password===e.target.value)
+              setIsConfirmed(password === e.target.value)
             }}
-            borderColor={isConfirmed?Colors.theme_light_green:Colors.theme_bold_red}
-            focusBorderColor={isConfirmed?Colors.theme_light_green:Colors.theme_bold_red}
+            borderColor={isConfirmed ? Colors.theme_light_green : Colors.theme_bold_red}
+            focusBorderColor={isConfirmed ? Colors.theme_light_green : Colors.theme_bold_red}
             _hover={{
-              borderColor:isConfirmed?Colors.theme_light_green:Colors.theme_bold_red
+              borderColor: isConfirmed ? Colors.theme_light_green : Colors.theme_bold_red
             }}
           />
           <InputRightElement w={'80px'}>
             <Button
               variant={'unstyled'}
-              onClick={()=>{
+              onClick={() => {
                 setIsConfirmShown(!isConfirmShown)
-                
               }}
             >
               {isConfirmShown ? 'Hide' : 'Show'}
             </Button>
           </InputRightElement>
         </InputGroup>
-              <Text
-              fontSize={'14px'}
-              color={Colors.theme_bold_red}
-              align={'right'}
-              m={'3px'}
-              >
-                 {confirmPassword===''||isConfirmed?'':'Must be same as password'}
-              </Text>
+        <Text
+          fontSize={'14px'}
+          color={Colors.theme_bold_red}
+          align={'right'}
+          m={'3px'}
+        >
+          {confirmPassword === '' || isConfirmed ? '' : 'Must be same as password'}
+        </Text>
+      </FormControl>
+      <FormControl>
+        <FormLabel>Upload Profile Picture</FormLabel>
+        <Input
+          type='file'
+          p={1.5}
+          accept='image/*'
+          onChange={(e) => { picUpload(e.target.files[0]) }}
+          border={'none'}
+        />
       </FormControl>
     </VStack>
   )
