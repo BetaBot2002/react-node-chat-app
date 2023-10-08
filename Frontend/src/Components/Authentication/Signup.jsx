329bc1f6589699260b295fcb39793901f35d0456
Sinchan Nandy <105705266+BetaBot2002@users.noreply.github.com>
2023-10-08 15:05:06 +0530

    Signup page initiated

--- Frontend/src/Components/Authentication/Signup.jsx
+++ Frontend/src/Components/Authentication/Signup.jsx
@@ -1,10 +1,42 @@
-import React from 'react'
+import { Button, FormControl, FormLabel, Input, InputGroup, InputRightElement, VStack } from '@chakra-ui/react'
+import React, { useState } from 'react'
 
 const Signup = () => {
+  const [isShown, setIsShown] = useState(false)
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const [confirmPassword, setConfirmPassword] = useState('')
   return (
-    <div>
-      Sign up
-    </div>
+    <VStack spacing={'15px'}>
+      <FormControl id='email' isRequired>
+        <FormLabel>Email</FormLabel>
+        <Input
+          placeholder='Enter Your Email'
+          onChange={(e) => setEmail(e.target.value)}
+        />
+      </FormControl>
+      <FormControl id='password' isRequired>
+        <FormLabel>Password</FormLabel>
+        <InputGroup>
+          <Input
+            type={isShown?'text':'password'}
+            placeholder='Enter Your Password'
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <InputRightElement w={'80px'}>
+            <Button
+              variant={'unstyled'}
+              onClick={()=>{
+                setIsShown(!isShown)
+              }}
+            >
+              {isShown ? 'Hide' : 'Show'}
+            </Button>
+          </InputRightElement>
+        </InputGroup>
+
+      </FormControl>
+    </VStack>
   )
 }
 
