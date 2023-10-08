96a9c39f7d2056dbfc2c5a69cb20afd8326092d1
Sinchan Nandy <105705266+BetaBot2002@users.noreply.github.com>
2023-10-09 01:25:46 +0530

    Login Component Customised

--- Frontend/src/Components/Authentication/Login.jsx
+++ Frontend/src/Components/Authentication/Login.jsx
@@ -1,10 +1,59 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Button, FormControl, FormLabel, Input, InputGroup, InputRightElement, Text, VStack } from '@chakra-ui/react'
+import { Colors } from '../../Utils/CSS-Variables'
+
 
 const Login = () => {
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const [isShown, setIsShown] = useState(false)
+  const submitHandler = () => {
+
+  }
+
   return (
-    <div>
-      Login
-    </div>
+    <VStack spacing={'15px'}>
+      <FormControl id='login-email' isRequired>
+        <FormLabel>Email</FormLabel>
+        <Input
+          placeholder='Enter Your Email'
+          onChange={(e) => setEmail(e.target.value)}
+          borderColor={Colors.theme_lavender}
+        />
+      </FormControl>
+      <FormControl id='login-password' isRequired>
+        <FormLabel>Password</FormLabel>
+        <InputGroup>
+          <Input
+            type={isShown ? 'text' : 'password'}
+            placeholder='Enter Your Password'
+            onChange={(e) => {
+              setPassword(e.target.value)
+            }}
+            borderColor={Colors.theme_lavender}
+          />
+          <InputRightElement w={'80px'}>
+            <Button
+              variant={'unstyled'}
+              onClick={() => {
+                setIsShown(!isShown)
+              }}
+            >
+              {isShown ? 'Hide' : 'Show'}
+            </Button>
+          </InputRightElement>
+        </InputGroup>
+      </FormControl>
+
+      <Button
+        w={'100%'}
+        colorScheme='purple'
+        marginTop={'15px'}
+        onClick={submitHandler}
+      >
+        Log In
+      </Button>
+    </VStack>
   )
 }
 
