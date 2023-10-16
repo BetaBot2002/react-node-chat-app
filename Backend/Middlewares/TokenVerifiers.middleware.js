3e7ab67814ca59891c081785fb003559d0480db2
Sinchan Nandy <105705266+BetaBot2002@users.noreply.github.com>
2023-10-16 20:21:19 +0530

    Refresh Token verifier middleware added

--- Backend/Middlewares/TokenVerifiers.middleware.js
+++ Backend/Middlewares/TokenVerifiers.middleware.js
@@ -16,6 +16,30 @@ const verifyAccessToken=(req,res,next)=>{
     }
 }
 
+const verifyRefreshToken=(req,res,next)=>{
+    const token=req.token
+    const isBlacklisted=false
+    if(!isBlacklisted){
+        const payload=verifyToken(token,"REFRESH")
+
+        if(!payload.status===200){
+            res.status(404).send({
+                message:payload.message
+            })
+        }else{
+            const {email} = payload
+            req.email=email
+            console.log(email)
+            next()
+        }
+    }else{
+        res.status(404).send({
+            message:`TOKEN EXPIRED`
+        })
+    }
+}
+
 export {
-    verifyAccessToken
+    verifyAccessToken,
+    verifyRefreshToken
 }
