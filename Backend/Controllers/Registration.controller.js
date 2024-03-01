2e8f112d9037cde28e19874d6311674a3142e231
Sinchan Nandy <105705266+BetaBot2002@users.noreply.github.com>
2024-03-02 01:42:42 +0530

    Registration Controller added and Custom Status Codes updated

---
+++ Backend/Controllers/Registration.controller.js
@@ -0,0 +1,20 @@
+import { registerUser } from "../Database/Users.query";
+import { CustomStatusCodes } from "../Utilities/CustomStatusCodes";
+
+const register=async (req,res)=>{
+    const {email,password}=req.body
+
+    try {
+        const user=await registerUser(email,password)
+        res.status(CustomStatusCodes.SUCCESS).send(user)
+    } catch (error) {
+        res.status(CustomStatusCodes.USER_ALREADY_EXISTS).send({
+            message:`USER_ALREADY_EXISTS:${email}`,
+            code:CustomStatusCodes.USER_ALREADY_EXISTS
+        })
+    }
+}
+
+export {
+    register
+}
--- Backend/Utilities/CustomStatusCodes.js
+++ Backend/Utilities/CustomStatusCodes.js
@@ -1,7 +1,8 @@
 const CustomStatusCodes={
     "SUCCESS":200,
     "INVALID_TOKEN":501,
-    "USER_NOT_FOUND":404
+    "USER_NOT_FOUND":404,
+    "USER_ALREADY_EXISTS":409
 }
 
 export {
