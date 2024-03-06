50a721125244ef70048fcc274beb4846779d971d
Sinchan Nandy <105705266+BetaBot2002@users.noreply.github.com>
2024-03-06 22:22:18 +0530

    Sign up form updated

--- Frontend/src/Components/Authentication/Signup.jsx
+++ Frontend/src/Components/Authentication/Signup.jsx
@@ -45,7 +45,7 @@ const Signup = () => {
     const upload_preset=import.meta.env.VITE_APP_CLOUDINARY_UPLOAD_PRESET
     const cloud_name=import.meta.env.VITE_APP_CLOUDINARY_CLOUD_NAME
     const api_base_url_image=import.meta.env.VITE_APP_CLOUDINARY_API_BASE_URL_IMAGE
-    
+
     formData.append("file",image)
     formData.append("upload_preset",upload_preset)
     formData.append("cloud_name",cloud_name)
@@ -64,7 +64,7 @@ const Signup = () => {
   }
 
   const submitHandler = () => {
-
+    console.log("sub")
   }
 
   return (
@@ -112,10 +112,10 @@ const Signup = () => {
               setConfirmPassword(e.target.value)
               setIsConfirmed(password === e.target.value)
             }}
-            borderColor={isConfirmed ? Colors.theme_light_green : Colors.theme_bold_red}
-            focusBorderColor={isConfirmed ? Colors.theme_light_green : Colors.theme_bold_red}
+            borderColor={isConfirmed && confirmPassword ? Colors.theme_light_green : Colors.theme_bold_red}
+            focusBorderColor={isConfirmed && confirmPassword ? Colors.theme_light_green : Colors.theme_bold_red}
             _hover={{
-              borderColor: isConfirmed ? Colors.theme_light_green : Colors.theme_bold_red
+              borderColor: isConfirmed && confirmPassword ? Colors.theme_light_green : Colors.theme_bold_red
             }}
           />
           <InputRightElement w={'80px'}>
@@ -135,7 +135,7 @@ const Signup = () => {
           align={'right'}
           m={'3px'}
         >
-          {confirmPassword === '' || isConfirmed ? '' : 'Must be same as password'}
+          {!confirmPassword || isConfirmed ? '' : 'Must be same as password'}
         </Text>
       </FormControl>
       <FormControl id='picture'>
@@ -155,6 +155,7 @@ const Signup = () => {
         marginTop={'15px'}
         onClick={submitHandler}
         isLoading={loading}
+        isDisabled={!email || !password || !confirmPassword || !isConfirmed}
       >
         Sign Up
       </Button>
