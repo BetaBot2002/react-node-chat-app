86cd6e25ae8a8851365270e363fa65935f9b4c51
Sinchan Nandy <105705266+BetaBot2002@users.noreply.github.com>
2024-03-07 02:25:25 +0530

    jwt helper added in frontend

---
+++ Frontend/src/Utils/jwt.helper.js
@@ -0,0 +1,43 @@
+import {isExpired} from 'react-jwt'
+
+let accessToken=''
+
+const setRefreshToken=(token)=>{
+    if(token===``) localStorage.removeItem(`refresh`)
+    else localStorage.setItem(`refresh`,token)
+}
+
+const getRefreshToken=()=>{
+    return localStorage.getItem('refresh')
+}
+
+const setAccessToken=(token)=>{
+    accessToken=token
+}
+
+const getAccessToken=async ()=>{
+    if(accessToken!=='' && !isExpired(accessToken)) return accessToken;
+    const api_url=`${import.meta.env.VITE_APP_BACKEND_API}/user/refresh`
+    try {
+        const response=await fetch(api_url,{
+            method:"get",
+            headers:{
+                'Authorization': `bearer ${getRefreshToken()}`
+            }
+
+        })
+        const data=await response.json()
+        setAccessToken(data.accessToken)
+        return data.accessToken
+    } catch (error) {
+        console.log(error)
+        return ''
+    }
+}
+
+export{
+    setAccessToken,
+    setRefreshToken,
+    getAccessToken,
+    getRefreshToken
+}
