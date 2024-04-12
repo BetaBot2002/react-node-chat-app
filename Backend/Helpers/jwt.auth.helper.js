0dd82da6e812e791e1ca2fae42ce8c8cf6505e26
Sinchan Nandy <105705266+BetaBot2002@users.noreply.github.com>
2024-04-12 16:39:08 +0530

    Refresh token expiry corrected

--- Backend/Helpers/jwt.auth.helper.js
+++ Backend/Helpers/jwt.auth.helper.js
@@ -5,7 +5,7 @@ const tokenExpiryTime = 15
 
 const signUser = (email) => {
     const AccessToken = jwt.sign({ email: email }, process.env.ACCESS_TOKEN_SECRET, { expiresIn: tokenExpiryTime })
-    const RefreshToken = jwt.sign({ email: email }, process.env.REFRESH_TOKEN_SECRET, { expiresIn: tokenExpiryTime })
+    const RefreshToken = jwt.sign({ email: email }, process.env.REFRESH_TOKEN_SECRET)
     return {
         accessToken: AccessToken,
         refreshToken: RefreshToken
