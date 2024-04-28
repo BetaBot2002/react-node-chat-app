ac4ecd1fd36d8d0f3b205407cb2ca8f7c2ec8f5e
Sinchan Nandy <105705266+BetaBot2002@users.noreply.github.com>
2024-04-28 18:18:21 +0530

    Create Group Chat added

--- Frontend/src/Components/Miscellaneous/GroupChatModal.jsx
+++ Frontend/src/Components/Miscellaneous/GroupChatModal.jsx
@@ -1,34 +1,121 @@
-import { Button, FormControl, Input, Modal, ModalBody, ModalCloseButton, ModalContent, ModalFooter, ModalHeader, ModalOverlay, useDisclosure, useToast } from '@chakra-ui/react'
+import { Box, Button, FormControl, Input, Modal, ModalBody, ModalCloseButton, ModalContent, ModalFooter, ModalHeader, ModalOverlay, Spinner, useDisclosure, useToast } from '@chakra-ui/react'
 import React from 'react'
 import { useState } from 'react'
 import { ChatState } from '../../Context/ChatProvider'
 import { Colors } from '../../Utils/CSS-Variables'
+import { getAccessToken } from '../../Utils/jwt.helper'
+import axios from 'axios'
+import UserListItem from '../UserComponents/UserListItem'
+import UserBadgeItem from '../UserComponents/UserBadgeItem'
 
 const GroupChatModal = ({ children }) => {
     const { isOpen, onOpen, onClose } = useDisclosure()
-    const [groupChatName, setGroupChatName] = useState()
+    const [groupChatName, setGroupChatName] = useState("")
     const [selectedUsers, setSelectedUsers] = useState([]);
     const [search, setSearch] = useState("");
     const [searchResults, setSearchResults] = useState([]);
+    const [loading, setLoading] = useState(false);
     const toast = useToast()
-    const { users, chats, setChats } = ChatState()
+    const { user, chats, setChats } = ChatState()
 
-    const handleSearch = (searchQuery) => {
+    const handleSearch = async (searchQuery) => {
         setSearch(searchQuery)
-        if(!searchQuery) return;
+        if (!searchQuery) return;
         try {
-            
+            setLoading(true)
+            const api_url = `${import.meta.env.VITE_APP_BACKEND_API}/user?search=${searchQuery}`
+            const config = {
+                headers: {
+                    "Content-type": "application/json",
+                    'Authorization': `bearer ${await getAccessToken()}`
+                }
+            }
+            const { data } = await axios.get(api_url, config)
+            console.log(data)
+            setLoading(false)
+            setSearchResults(data)
         } catch (error) {
-            
+            toast({
+                title: "Error Occured!",
+                description: "Failed to Load the Search Results",
+                status: "error",
+                duration: 5000,
+                isClosable: true,
+                position: "bottom-left",
+            });
         }
     }
 
-    const handleSubmit = (value) => {
+    const addToSelectedUsers = (user) => {
+        if (selectedUsers.includes(user)) {
+            toast({
+                title: "User already added",
+                status: "warning",
+                duration: 5000,
+                isClosable: true,
+                position: "top",
+            });
+            return;
+        }
 
+        setSelectedUsers([...selectedUsers, user]);
+        setSearchResults(searchResults.filter(searchedUser => user.id !== searchedUser.id))
+    }
+
+    const handleSubmit = async () => {
+        if (!groupChatName || !selectedUsers) {
+            toast({
+                title: "Please fill all the feilds",
+                status: "warning",
+                duration: 5000,
+                isClosable: true,
+                position: "top",
+            });
+            return;
+        }
+
+        try {
+            const api_url = `${import.meta.env.VITE_APP_BACKEND_API}/chat/group`
+            const body = {
+                name: groupChatName,
+                users: JSON.stringify(selectedUsers.map((u) => u.id)),
+            }
+            const config = {
+                headers: {
+                    "Content-type": "application/json",
+                    'Authorization': `bearer ${await getAccessToken()}`
+                }
+            }
+            const { data } = await axios.post(api_url, body, config)
+            console.log(data)
+            setChats([data, ...chats])
+            onClose();
+            toast({
+                title: "New Group Chat Created!",
+                status: "success",
+                duration: 5000,
+                isClosable: true,
+                position: "bottom",
+            });
+        } catch (error) {
+            toast({
+                title: "Error Occured!",
+                description: "Failed to create group",
+                status: "error",
+                duration: 5000,
+                isClosable: true,
+                position: "bottom-left",
+            });
+        }
+    }
+
+    const handleDelete = (user) => {
+        setSelectedUsers(selectedUsers.filter(selectedUser => user.id !== selectedUser.id))
+        setSearchResults([...searchResults, user]);
     }
     return (
         <>
-            <Button onClick={onOpen}>{children}</Button>
+            <span onClick={onOpen}>{children}</span>
 
             <Modal isOpen={isOpen} onClose={onClose}>
                 <ModalOverlay />
@@ -61,6 +148,21 @@ const GroupChatModal = ({ children }) => {
                                 onChange={(e) => { handleSearch(e.target.value) }}
                             />
                         </FormControl>
+                        <Box w="100%" display="flex" flexWrap="wrap">
+                            {selectedUsers.map((selectedUser) => (
+                                <UserBadgeItem
+                                    key={selectedUser.id}
+                                    user={selectedUser}
+                                    handleDelete={() => handleDelete(selectedUser)}
+                                    admin={user}
+                                />
+                            ))}
+                        </Box>
+                        {loading && <Spinner m={'auto'} display={'flex'} />}
+                        {!loading && searchResults.slice(0, 4).map(user => (
+                            <UserListItem key={user.id} user={user} handleClick={() => { addToSelectedUsers(user) }} />
+                        ))}
+
                     </ModalBody>
                     <ModalFooter>
                         <Button bg={Colors.theme_dark_lavender} color={Colors.theme_light} mr={3} onClick={handleSubmit}>
---
+++ Frontend/src/Components/UserComponents/UserBadgeItem.jsx
@@ -0,0 +1,27 @@
+import { Badge } from '@chakra-ui/react'
+import React from 'react'
+import { Colors } from '../../Utils/CSS-Variables';
+import { CloseIcon } from '@chakra-ui/icons';
+
+const UserBadgeItem = ({ user, handleDelete, admin }) => {
+    return (
+        <Badge
+            px={2}
+            py={1}
+            borderRadius={"lg"}
+            m={1}
+            mb={2}
+            variant={"solid"}
+            fontSize={12}
+            backgroundColor={Colors.theme_dark_lavender}
+            cursor={"pointer"}
+            onClick={handleDelete}
+        >
+            {user.name}
+            {admin.id === user.id && <span> (Admin)</span>}
+            <CloseIcon pl={1} />
+        </Badge>
+    );
+};
+
+export default UserBadgeItem
