757d1158fee3b8ea4ecd0e952e8647adaa67f33d
Sinchan Nandy <105705266+BetaBot2002@users.noreply.github.com>
2024-05-01 00:04:56 +0530

    Chats rendering ui done

--- Frontend/src/Components/ScrollableChat.jsx
+++ Frontend/src/Components/ScrollableChat.jsx
@@ -1,11 +1,50 @@
+import { Avatar, Box, Tooltip } from '@chakra-ui/react'
 import React from 'react'
+import ScrollableFeed from 'react-scrollable-feed'
+import { ChatState } from '../Context/ChatProvider'
+import { isLastMessage, isSameSender, isSameSenderMargin, isSameUser } from '../Utils/chat.helper'
 
-const ScrollableChat = ({messages}) => {
-  return (
-    <div>
-      
-    </div>
-  )
+const ScrollableChat = ({ messages }) => {
+    const { user } = ChatState()
+    return (
+        <ScrollableFeed>
+            {messages && messages.map((message, index) => (
+                <Box display={"flex"} key={message.id}>
+                    {
+                        (isSameSender(messages, message, index, user.id) || isLastMessage(messages, index, user.id)) &&
+                        (
+                            <Tooltip
+                                label={message.sender.name}
+                                placement='bottom-start'
+                                hasArrow
+                            >
+                                <Avatar
+                                    mt="7px"
+                                    mr={1}
+                                    size="sm"
+                                    cursor="pointer"
+                                    name={message.sender.name}
+                                    src={message.sender.profilePic}
+                                />
+                            </Tooltip>
+                        )
+                    }
+                    <span
+                        style={{
+                            backgroundColor: `${message.sender.id === user.id ? "#BEE3F8" : "#B9F5D0"}`,
+                            borderRadius: "20px",
+                            padding: "5px 15px",
+                            maxWidth: "75%",
+                            marginLeft: isSameSenderMargin(messages, message, index, user.id),
+                            marginTop: isSameUser(messages, message, index) ? 3 : 10
+                        }}
+                    >
+                        {message.content}
+                    </span>
+                </Box>
+            ))}
+        </ScrollableFeed>
+    )
 }
 
 export default ScrollableChat
--- Frontend/src/Utils/chat.helper.js
+++ Frontend/src/Utils/chat.helper.js
@@ -1,7 +1,49 @@
-const getSender=(loggedUser,users)=>{
+const getSender = (loggedUser, users) => {
     return users[0]?.id === loggedUser?.id ? users[1] : users[0];
 }
 
+const isSameSender = (messages, currentMessage, index, loggedUserId) => {
+    return (
+        index < messages.length - 1 &&
+        (messages[index + 1].sender.id !== currentMessage.sender.id ||
+            messages[index + 1].sender.id === undefined) &&
+        messages[index].sender.id !== loggedUserId
+    );
+};
+
+const isLastMessage = (messages, index, loggedUserId) => {
+    return (
+        index === messages.length - 1 &&
+        messages[messages.length - 1].sender.id !== loggedUserId &&
+        messages[messages.length - 1].sender.id
+    );
+};
+
+const isSameSenderMargin = (messages, currentMessage, index, loggedUserId) => {
+    if (
+        index < messages.length - 1 &&
+        messages[index + 1].sender.id === currentMessage.sender.id &&
+        messages[index].sender.id !== loggedUserId
+    )
+        return 33;
+    else if (
+        (index < messages.length - 1 &&
+            messages[index + 1].sender.id !== currentMessage.sender.id &&
+            messages[index].sender.id !== loggedUserId) ||
+        (index === messages.length - 1 && messages[index].sender.id !== loggedUserId)
+    )
+        return 0;
+    else return "auto";
+};
+
+const isSameUser = (messages, currentMessage, index) => {
+    return index > 0 && messages[index - 1].sender.id === currentMessage.sender.id;
+};
+
 export {
-    getSender
+    getSender,
+    isSameSender,
+    isLastMessage,
+    isSameSenderMargin,
+    isSameUser
 }
