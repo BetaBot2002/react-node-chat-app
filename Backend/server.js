a5932737b14b4244b6398ec74e1b0fefcf4f9467
Sinchan Nandy <105705266+BetaBot2002@users.noreply.github.com>
2024-05-06 00:14:59 +0530

    Socket setup initialized in backend and Socket.io client installed in frontend

--- Backend/server.js
+++ Backend/server.js
@@ -5,23 +5,37 @@ import { userRouter } from "./Routes/User.route.js"
 import { errorRoute, notFound } from "./Middlewares/ErrorHandler.middleware.js"
 import { chatRouter } from "./Routes/Chats.route.js"
 import { messageRouter } from "./Routes/Message.route.js"
+import { Server } from "socket.io"
 
 dotenv.config()
 
-const app=express()
+const app = express()
 
-const PORT= process.env.PORT || 3001
+const PORT = process.env.PORT || 3001
 
 app.use(cors())
 app.use(express.json())
-app.use(express.urlencoded({extended:false}))
+app.use(express.urlencoded({ extended: false }))
 
-app.use("/user",userRouter)
-app.use("/chat",chatRouter)
-app.use("/message",messageRouter)
+app.use("/user", userRouter)
+app.use("/chat", chatRouter)
+app.use("/message", messageRouter)
 app.use(notFound)
 app.use(errorRoute)
 
-app.listen(PORT,()=>{
+const server = app.listen(PORT, () => {
     console.log(`http://127.0.0.1:${PORT}`)
 })
+
+const io = new Server(server, {
+    pingTimeout: 60000,
+    cors: {
+        origin: "*",
+    },
+});
+
+io.on("connection",(socket)=>{
+    console.log("Connected to socket");
+})
+
+
